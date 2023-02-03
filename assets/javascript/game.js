3a2404615ba9730105a693a2ea6f2f3e74a54309
Windsor <windsorjunior@hotmail.com>
2023-02-03 09:58:54 -0300

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,89 +1,84 @@
 // Global Variables
-
 var secs10 = 11;
 var secs60 = 5;
 var secs120 = 10;
-var speed = 6;
+var speed = 0;
 
 //Hide timer and speed info
 $('#timer-info').hide();
 
-
-// Timer Functions
-    function timer10() {
-
-         const timeinterval = setInterval(function() {
-
-            // Countdown seconds
-            secs10--;
-
-            // Display timer on screen
-            $('.timer-zone').text(secs10).addClass("red10");
-
-            // If the seconds reach zero, stop timer
-            if (secs10 == 0) {
-
-            clearInterval(timeinterval);
-              
-            }
-           
-        }, 1000);
-
-    }
-
-
-    function timer60() {
-
-         const timeinterval = setInterval(function() {
-
-            // Countdown seconds
-            secs60--;
-
-            // Display timer on screen
-            $('.timer-zone').text(secs60);
-
-            // If the seconds reach zero, stop timer
-            if (secs60 == 0) {
-
-            clearInterval(timeinterval);
-
-            timer10();
-                
-            } 
-
-        }, 1000);
-
-    }
-
-
-     function timer120() {
-
-         const timeinterval = setInterval(function() {
-
-            // Countdown seconds
-            secs120--;
-
-            // Display timer on screen
-            $('.timer-zone').text(secs120);
-
-            // If the seconds reach zero, stop timer
-            if (secs120 == 0) {
-
-            clearInterval(timeinterval);
-
-            timer10();
-                
-            } 
-
-        }, 1000);
-
-    }
-
-    // Start Screen Button
+// Start Program on button clicked
 $(".btn-start").click(function(){
 
 	$('#timer-info').show();
 	$('#start-button').hide();	
-	timer60();
+	step1();
 
 });
+
+//============================================================
+
+function step1(){
+
+	// Define Speed for this Step and Print It
+	var speed = 8;
+	$('.speed-zone').text(speed);
+	
+	// Set up Timer
+	const timeinterval = setInterval(function() {
+   	secs60--; // Countdown seconds
+   	$('.timer-zone').text(secs60); // Display timer on screen
+   	
+   	if (secs60 == 0) { // When time finishes
+   		//Stop Timer
+       	clearInterval(timeinterval); 
+        // Define Speed for the next Step and Print It
+		var speed = 12;
+		$('.speed-zone').text(speed);
+		// Start the 10 second timer
+		timer10();
+     } 
+   }, 1000);
+}
+
+function step2() {
+
+	$('.timer-zone').text("ALRIGHT!");  
+
+}
+
+
+//=========================================================
+
+// Timer Functions
+function timer10() {
+   const timeinterval = setInterval(function() {
+   // Countdown seconds
+   secs10--;
+   // Display timer on screen
+   $('.timer-zone').text(secs10).addClass("red10");  
+
+   if (secs10 == 0) { 
+   	   clearInterval(timeinterval); 
+   	   step2();
+   	} 		
+
+}, 1000);
+  
+}
+
+function timer60() {
+   const timeinterval = setInterval(function() {
+   // Countdown seconds
+   secs60--;
+   // Display timer on screen
+   $('.timer-zone').text(secs60);  }, 1000);
+}
+
+function timer120() {
+   const timeinterval = setInterval(function() {
+   // Countdown seconds
+   secs120--;
+   // Display timer on screen
+   $('.timer-zone').text(secs120);  }, 1000);
+}
